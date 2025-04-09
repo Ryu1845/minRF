5610eb44d8e67f3c77aed921694aacf55b5fb2d2
Sofian Mejjoute <77058942+Ryu1845@users.noreply.github.com>
2025-04-09 21:36:58 +0200

    rm batch sze in eval

--- rf.py
+++ rf.py
@@ -147,7 +147,7 @@ if __name__ == "__main__":
             gif = []
             for idx, image in enumerate(images):
                 fig, ax = plt.subplots()
-                img = librosa.display.specshow(image.cpu().numpy(), ax=ax)
+                img = librosa.display.specshow(image[0].cpu().numpy(), ax=ax)
                 fig.colorbar(img, ax=ax)
                 fig.savefig(f'contents/sample_{epoch}_{idx}.png')
         rf_model.train()

06bbfdfcb8ee1f7d578609cc5c61c24f8ec8f73f
Sofian Mejjoute <77058942+Ryu1845@users.noreply.github.com>
2025-04-09 21:29:24 +0200

    fix viz

--- rf.py
+++ rf.py
@@ -172,7 +172,7 @@ if __name__ == "__main__":
             gif = []
             for idx, image in enumerate(images):
                 fig, ax = plt.subplots()
-                img = librosa.display.specshow(S_db, ax=ax)
+                img = librosa.display.specshow(image, ax=ax)
                 fig.colorbar(img, ax=ax)
                 fig.savefig(f'contents/sample_{epoch}_{idx}.png')
 
